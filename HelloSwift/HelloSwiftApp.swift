52be3fb0bd2fc1209215e2dac060935eefbd009d
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-11-10 09:35:52 +0900

    Initial Commit

---
+++ HelloSwift/HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftApp.swift
+//  HelloSwift
+//
+//  Created by 磯野圭希 on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
