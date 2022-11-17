0a67aa2fc346235a97a137e37688b2e7b97c8e1d
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-11-17 10:22:22 +0900

    :sparkles: add Button & OutputText Change

--- HelloSwift/ContentView.swift
+++ HelloSwift/ContentView.swift
@@ -1,12 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State var outputText = "Hello, world!"
+  
   var body: some View {
     VStack {
       Image(systemName: "globe")
         .imageScale(.large)
         .foregroundColor(.accentColor)
-      Text("Hello, swift!")
+      Text(outputText)
+        .font(.largeTitle)
+        .fontWeight(.bold)
+        .foregroundColor(Color.black)
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+      Button("切り替えボタン"){
+        if outputText == "Hi, Swift!" {
+          outputText = "Hello, world!"
+        } else {
+          outputText = "Hi, Swift!"
+        }
+      }
+      .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+      .foregroundColor(Color.white)
+      .background(Color.blue)
+      .clipShape(Capsule())
     }
     .padding()
   }
